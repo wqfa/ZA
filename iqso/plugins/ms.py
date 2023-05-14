4055f95e166ba5246da0b385f76dfe1b01679d74
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-05-14 12:28:03 +0300

    Update ms.py

--- iqso/plugins/ms.py
+++ iqso/plugins/ms.py
@@ -35,7 +35,7 @@ from telethon.tl.types import ChannelParticipantCreator as owner
 from telethon.tl.types import UserStatusOffline as off
 from telethon.tl.types import UserStatusOnline as onn
 from telethon.tl.types import UserStatusRecently as rec
-from Arab import iqthon
+from iqso import iqthon
 from Arab.core.logger import logging
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
