0536f7d3eb690563daec91e303f5fada7194d5e3
XF <118357460+wqfa@users.noreply.github.com>
2023-08-08 06:02:47 +0300

    Update id.py

--- IqArab/plugins/id.py
+++ IqArab/plugins/id.py
@@ -15,7 +15,7 @@ from ..sql_helper.globals import gvarstatus
 from . import spamwatch
 plugin_category = "@iqthon"
 LOGS = logging.getLogger(__name__)
-iqthon_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "╮•• مـعلومات الـشخص من بوت تليثون العرب"
+iqthon_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "╮•• مـعلومات الـشخص من بوت سورس الامراء"
 iqthonM = gvarstatus("CUSTOM_ALIVE_EMOJI") or "✦"
 iqthonF = gvarstatus("CUSTOM_ALIVE_FONT") or "★•┉ ┉ ┉ ┉ ┉ ┉  ┉ ┉ ┉ ┉•★"
 
