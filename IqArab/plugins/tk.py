380bc1d046ed0529761925dc48060aebba1b284c
XF <118357460+wqfa@users.noreply.github.com>
2023-08-08 06:11:44 +0300

    Update tk.py

--- IqArab/plugins/tk.py
+++ IqArab/plugins/tk.py
@@ -162,12 +162,12 @@ async def spammer(event):
     try:
         sleeptimet = sleeptimem = float(input_str[0])
     except Exception:
-        return await edit_delete(            event, "عذرا طريقه كتابة الأمر خطأ - شرح الارسال الوقتي او مؤقت للكروبات هنا : https://t.me/L3LL3/4483"        )
+        return await edit_delete(            event, "عذرا طريقه كتابة الأمر خطأ - شرح الارسال الوقتي او مؤقت للكروبات هنا : https://t.me/T9_10"        )
     cat = input_str[1:]
     try:
         int(cat[0])
     except Exception:
-        return await edit_delete(            event, "عذرا طريقه كتابة الأمر خطأ - شرح الارسال الوقتي او مؤقت للكروبات هنا : https://t.me/L3LL3/4483"        )
+        return await edit_delete(            event, "عذرا طريقه كتابة الأمر خطأ - شرح الارسال الوقتي او مؤقت للكروبات هنا : https://t.me/T9_10"        )
     await event.delete()
     addgvar("spamwork", True)
     await spam_function(event, reply, cat, sleeptimem, sleeptimet, DelaySpam=True)
